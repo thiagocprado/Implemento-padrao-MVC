af93836ed44276469cfe7f019c9784fcf5f62516
Thiago Prado <thiago.prado@ernetwork.com.br>
2021-01-07 18:03:55 -0300

    Subindo projeto parte 1

---
+++ client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,15 @@
+class ListaNegociacoes {
+
+  constructor() {
+    this._negociacoes = [];
+  }
+
+  adiciona(negociacao) {
+    this._negociacoes.push(negociacao);
+  }
+
+  get negociacoes() {
+    // deixaremos apenas para visualização
+    return [].concat(this._negociacoes);
+  }
+}
