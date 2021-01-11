33e3c1f4beacb4ee7490d632371c76b42432f695
Thiago Prado <thiago.prado@ernetwork.com.br>
2021-01-11 18:02:34 -0300

    Subindo projeto

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,23 @@
 class ListaNegociacoes {
 
-    constructor() {
+    constructor(armadilha) {
 
         this._negociacoes = [];
+        this._armadilha = armadilha;
     }
 
     adiciona(negociacao) {
-
         this._negociacoes.push(negociacao);
+        this._armadilha(this)
     }
 
     get negociacoes() {
         // deixaremos apenas para visualização
         return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+        this._negociacoes = [];
+        this._armadilha(this)
     }
 }
